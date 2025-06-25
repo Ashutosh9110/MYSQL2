d3b41e702347e7ca05b2af9165e2079921fdc820
Ashutosh Adhikari <veni_vidi.vici@outlook.com>
2025-06-25 16:45:24 +0530

    Designing the Initial Database Schema for a Bus Booking System(Task)

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require("express")
 const mysql = require("mysql2")
-
 const app = express()
+
 
 const connection = mysql.createConnection({
   host:"localhost",
-  user: "root",
+  user:"root",
   password: "1234",
-  database: "testdb"
+  database: "test"
 })
-
 
 connection.connect((err) => {
   if(err) {
@@ -19,28 +18,76 @@ connection.connect((err) => {
   console.log("Connection has been created");
 })
 
-const createTable = `create table uuuu(
+
+
+const createUser = `create table user(
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255),
   email VARCHAR(255)
 )`
 
-connection.execute(createTable, (err) => {
+
+connection.execute(createUser, (err) => {
+  if(err) {
+    console.log(err);
+    connection.end()
+    return 
+  }
+})
+
+
+
+const createBuses = `create table buse(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  busNumber VARCHAR(255),
+  totalSeats INT,
+  availableSeats INT
+)`
+
+connection.execute(createBuses, (err) => {
   if(err) {
     console.log(err);
     connection.end()
     return
   }
-  console.log("Table is created");
 })
 
 
-app.get("/", (req, res) => {
-  res.send("Hello World")
+const createBookings = `create table booking(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  seatNumber INT
+)`
+
+connection.execute(createBookings, (err) => {
+  if(err) {
+    console.log(err);
+    connection.end()
+    return
+  }
+  console.log("Table has been created");
 })
 
+
+
+const createPayments = `create table payment(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  amountPaid INT,
+  paymentStatus VARCHAR(255)
+)`
+
+
+connection.execute(createPayments, (err) => {
+  if(err) {
+    console.log(err);
+    connection.end()
+    return
+  }
+  console.log("Table has been created");
+})
 
 
 app.listen(3000, () => {
   console.log("Server running at 3000");
 })
+
+
